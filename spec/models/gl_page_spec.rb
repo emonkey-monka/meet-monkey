240ed0c56d36867be329ee54deb7b4e538cf40eb
kazuhide hane <hidekkobonba1213@gmail.com>
2017-12-11 06:41:31 +0000

    model rspec test

--- spec/models/gl_page_spec.rb
+++ spec/models/gl_page_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe GlPage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#   pending "add some examples to (or delete) #{__FILE__}"
+  it "check calc_distance" do
+      glpage = GlPage.create(latitude: 0, longitude: 0)
+      glpage.calc_distance(15, 15)
+      expect(glpage.distance).to eq 2347.7927573803795
+    end
 end
