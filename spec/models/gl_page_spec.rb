0078b324cbe3229d504d859954b6d867b7d5a6a0
kazuhide hane <hidekkobonba1213@gmail.com>
2017-12-11 06:58:20 +0000

    rspec fix

--- spec/models/gl_page_spec.rb
+++ spec/models/gl_page_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe GlPage, type: :model do
-  
-  let(:glpage) { GlPage.create(latitude: 0, longitude: 0) }
-  
-  it "check calc_distance" do
-    glpage.calc_distance(15, 15)
-    expect(glpage.distance).to eq 2347.7927573803795
-  end
+  describe '#calc_distance' do
+    let(:glpage) { GlPage.create(latitude: 0, longitude: 0) }
+    let(:latitude) { 15 }
+    let(:longitude) { 15 }
+
+    before { GlPage.calc_distance }
+    subject(:distance) { glpage.distance }
+    it { expect(distance).to eq 2347.7927573803795 }
+  end 
 end
