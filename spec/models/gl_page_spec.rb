02bc9b6d413464729127d1fb90e7a79a040ad71e
hidekkobonba <hidekkobonba1213@gmail.com>
2017-12-11 16:10:02 +0900

    calc_distance メソッドのrspecテスト (#16)
    * model rspec test
    
    * let
    
    * rspec fix
    
    * refix

--- spec/models/gl_page_spec.rb
+++ spec/models/gl_page_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe GlPage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#calc_distance' do
+    let(:glpage) { GlPage.create(latitude: 0, longitude: 0) }
+    let(:latitude) { 15 }
+    let(:longitude) { 15 }
+
+    before { glpage.calc_distance(latitude, longitude) }
+    subject(:distance) { glpage.distance }
+    it { expect(distance).to eq 2347.7927573803795 }
+  end 
 end
