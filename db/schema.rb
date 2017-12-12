7207d57efa0015767fb0bf7a1cc374b1f3eec3ef
kazuhide hane <hidekkobonba1213@gmail.com>
2017-12-12 04:55:59 +0000

    rebasemaster

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212010939) do
+ActiveRecord::Schema.define(version: 20171212012256) do
 
   create_table "gl_pages", force: :cascade do |t|
     t.decimal "latitude", precision: 9, scale: 6
